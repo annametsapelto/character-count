90f902ab9891978e77e8e81e97fde3e420ea3d10
Anna Metsäpelto <anna.metsapelto@gmail.com>
2023-09-21 21:15:17 +0300

    Create react app and clean unneeded

---
+++ character-count/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Character Count App</h1>
+        <p>This app can be used to count all the characters in a sentence.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
