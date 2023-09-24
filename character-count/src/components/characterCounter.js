7bba7f0412142362dd9d2480c81a36c112655dc1
Anna Metsäpelto <anna.metsapelto@gmail.com>
2023-09-24 22:27:19 +0300

    Fix the loop bug

--- character-count/src/components/characterCounter.js
+++ character-count/src/components/characterCounter.js
@@ -3,16 +3,33 @@ import { useState, useEffect } from "react";
 function CharacterCounter({sentence}) {
 
     const [characterList, setCharacterList] = useState([]);
-    useEffect(() => {
 
-    }, sentence);
+    useEffect(() => {
+        const charCount = {};
+
+        for (let i = 0; i < sentence.length; i++) {
+            const character = sentence.charAt(i);
+
+            if(charCount[character]) {
+                charCount[character]++;
+            } else {
+                charCount[character] = 1;
+            }
+        }
+        const charList = Object.keys(charCount).map(char => ({
+            char, count: charCount[char]
+        }));
+        setCharacterList(charList);
+    }, [sentence]);
 
     return(
         <>
             <p>The sentence is: {sentence}</p>
             <p>It has the following characters:</p>
             <ul>
-
+                { characterList.map((item) => (
+                    <li key={item.char}>{item.char} {item.count}</li>
+                ))}
             </ul>
         </>
     )
